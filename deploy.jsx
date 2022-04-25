e8c78225daee62fb13c15c3deb7314334f2058cc
Luke Wilson <l2wilson94@gmail.com>
2022-04-25 18:10:11 +0100

    post cheep?

--- deploy.jsx
+++ deploy.jsx
@@ -45,10 +45,7 @@ const db = getFirestore(firebaseApp);
 
 const users = new Map();
 
-const app = new Application();
 const router = new Router();
-
-app.use(virtualStorage());
 
 router.get("/", (ctx) => {
   ctx.response.body = render(<Feed />).body;
@@ -67,9 +64,53 @@ router.get("/cheeps", async (ctx) => {
   }
 });
 
+function isCheep(value) {
+  if (typeof value !== "object") return false;
+  if (value === null) return false;
+  if (!("id" in value)) return false;
+  if (!("text" in value)) return false;
+  return true;
+}
+
+router.post("/cheep", async (ctx) => {
+  const body = ctx.request.body();
+  if (body.type !== "json") {
+    ctx.throw(Status.BadRequest, "Must be a JSON document");
+  }
+  const cheep = await body.value;
+  if (!isCheep(cheep)) {
+    ctx.throw(Status.BadRequest, "Cheep was not well formed");
+  }
+  addDoc(collection(db, "cheeps"), cheep);
+  ctx.response.status = Status.NoContent;
+});
+
 router.get("/(.*)", (ctx) => {
   ctx.response.body = render(<NotFound />).body;
   ctx.response.type = "text/html";
+});
+
+const app = new Application();
+app.use(virtualStorage());
+
+app.use(async (ctx, next) => {
+  const signedInUid = ctx.cookies.get("LOGGED_IN_UID");
+  const signedInUser = signedInUid != null ? users.get(signedInUid) : undefined;
+  if (!signedInUid || !signedInUser || !auth.currentUser) {
+    const creds = await signInWithEmailAndPassword(
+      auth,
+      Deno.env.get("FIREBASE_USERNAME"),
+      Deno.env.get("FIREBASE_PASSWORD")
+    );
+    const { user } = creds;
+    if (user) {
+      users.set(user.uid, user);
+      ctx.cookies.set("LOGGED_IN_UID", user.uid);
+    } else if (signedInUser && signedInUid.uid !== auth.currentUser?.uid) {
+      await auth.updateCurrentUser(signedInUser);
+    }
+  }
+  return next();
 });
 
 app.use(router.routes());
