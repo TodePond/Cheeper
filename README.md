e4dd582dbf6558a59e456152e503a1e2e2a7b160
Luke Wilson <l2wilson94@gmail.com>
2022-04-25 17:07:22 +0100

    cheepstore

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Cheeper
+
 a work-in-progress place I can post my stuff for people who don't wanna use twitter: [cheeper.cool](https://cheeper.cool/)
 
 to run locally:
+
 ```
-deno run --allow-net=:8000 --watch --no-check deploy.jsx
+deno run --allow-net --allow-env --allow-read=. --watch --no-check deploy.jsx
 ```
--- deploy.jsx
+++ deploy.jsx
@@ -7,14 +7,56 @@ import { serve } from "https://deno.land/std@0.133.0/http/server.ts";
 import { router } from "https://crux.land/router@0.0.11";
 import { h, ssr } from "https://crux.land/nanossr@0.0.4";
 
-const render = (component) => ssr(() => <App>{component}</App>);
+import "https://deno.land/x/xhr@0.1.2/mod.ts";
+import { installGlobals } from "https://deno.land/x/virtualstorage@0.1.0/mod.ts";
+import { virtualStorage } from "https://deno.land/x/virtualstorage@0.1.0/middleware.ts";
 
-serve(router(
-  {
-    "/": () => render(<Feed />),
-  },
-  () => render(<NotFound />),
-));
+// Env
+import "https://deno.land/std@0.136.0/dotenv/load.ts";
+
+// Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  deleteDoc,
+  getDocs,
+  query,
+  where,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+
+const render = (component) => ssr(() => <App>{component}</App>);
+installGlobals();
+
+const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG"));
+console.log(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig, "example");
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+const users = new Map();
+
+serve(
+  router(
+    {
+      "/": () => render(<Feed />),
+      "/cheeps": async () => {
+        const cheepsCol = collection(db, "cheeps");
+        const querySnapshot = await getDocs(cheepsCol);
+        new Response({
+          body: querySnapshot.docs.map((doc) => doc.data()),
+          type: "json",
+        });
+      },
+    },
+    () => render(<NotFound />)
+  )
+);
 
 function App({ children }) {
   return (
@@ -28,9 +70,11 @@ function App({ children }) {
 function NavBar() {
   return (
     <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
-     <div class="mb-2 sm:mb-0">
-      <a href="/" class="text-2xl no-underline hover:text-indigo-800">Cheeper</a>
-     </div>
+      <div class="mb-2 sm:mb-0">
+        <a href="/" class="text-2xl no-underline hover:text-indigo-800">
+          Cheeper
+        </a>
+      </div>
     </nav>
   );
 }
@@ -56,12 +100,16 @@ function NotFound() {
           <p class="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
           <div class="sm:ml-6">
             <div class="sm:border-l sm:border-gray-200 sm:pl-6">
-              <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Page not found</h1>
-              <p class="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                Page not found
+              </h1>
+              <p class="mt-1 text-base text-gray-500">
+                Please check the URL in the address bar and try again.
+              </p>
             </div>
           </div>
         </main>
       </div>
     </div>
-  )
+  );
 }
