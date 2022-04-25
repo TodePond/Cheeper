a453ce8597ae6accad71bfe5534a7277d0d19a2f
Luke Wilson <l2wilson94@gmail.com>
2022-04-25 14:06:04 +0100

    jsx

---
+++ deploy.jsx
@@ -0,0 +1,33 @@
+/** @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX</title>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>
+  );
+}
+
+function handler(req) {
+  const html = renderSSR(<App />);
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+}
+
+console.log("Listening on http://localhost:8000");
+serve(handler);
