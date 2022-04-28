d42edcefed1a5f10107dac409a6a18fc0c52d273
Luke Wilson <l2wilson94@gmail.com>
2022-04-28 12:45:14 +0100

    ctx

--- deploy.jsx
+++ deploy.jsx
@@ -34,7 +34,7 @@ import {
 // Oak
 import { Application, Router, Status } from "https://deno.land/x/oak@v7.7.0/mod.ts"
 
-const render = (ctx, component) => ssr(() => <App>{component}</App>).body
+const render = (ctx, component) => ssr(() => <App ctx={ctx}>{component}</App>).body
 installGlobals()
 
 const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG"))
@@ -42,27 +42,27 @@ const firebaseApp = initializeApp(firebaseConfig, "example")
 const auth = getAuth(firebaseApp)
 const db = getFirestore(firebaseApp)
 
-const isLoggedIn = () => {
+const isLoggedIn = (ctx) => {
 	return auth.currentUser !== null
 }
 
 const router = new Router()
-router.get("/", async ctx => {
+router.get("/", async (ctx) => {
 	const cheepCollection = collection(db, "cheeps")
 	const q = query(cheepCollection, orderBy("time", "desc"), limit(10))
 	const querySnapshot = await getDocs(q)
 	const cheepDocuments = querySnapshot.docs
-	const cheeps = cheepDocuments.map(doc => doc.data())
+	const cheeps = cheepDocuments.map((doc) => doc.data())
 
 	ctx.response.body = render(ctx, <Feed cheeps={cheeps} />)
 	ctx.response.type = "text/html"
 })
 
-router.get("/cheeps", async ctx => {
+router.get("/cheeps", async (ctx) => {
 	const cheepsCol = collection(db, "cheeps")
 	try {
 		const querySnapshot = await getDocs(cheepsCol)
-		ctx.response.body = querySnapshot.docs.map(doc => doc.data())
+		ctx.response.body = querySnapshot.docs.map((doc) => doc.data())
 		ctx.response.type = "json"
 	} catch (e) {
 		ctx.response.body = render(ctx, <NotAllowed />)
@@ -70,7 +70,7 @@ router.get("/cheeps", async ctx => {
 	}
 })
 
-const isCheep = value => {
+const isCheep = (value) => {
 	if (typeof value !== "object") return false
 	if (value === null) return false
 	if (!("file" in value)) return false
@@ -79,21 +79,21 @@ const isCheep = value => {
 	return true
 }
 
-router.get("/cheep", async ctx => {
-	if (!isLoggedIn()) {
+router.get("/cheep", async (ctx) => {
+	if (!isLoggedIn(ctx)) {
 		ctx.response.redirect("/login")
 	}
 	ctx.response.body = render(ctx, <NewCheep />)
 	ctx.response.type = "text/html"
 })
 
-router.post("/cheep", async ctx => {
+router.post("/cheep", async (ctx) => {
 	const body = ctx.request.body()
 	if (body.type !== "form") {
 		ctx.throw(Status.BadRequest, "Cheep was not well formed")
 		return
 	}
-	if (!isLoggedIn()) {
+	if (!isLoggedIn(ctx)) {
 		ctx.throw(Status.BadRequest, "You need to login to cheep")
 		return
 	}
@@ -111,18 +111,18 @@ router.post("/cheep", async ctx => {
 	ctx.response.redirect("/")
 })
 
-router.get("/logout", async ctx => {
+router.get("/logout", async (ctx) => {
 	auth.signOut()
 	ctx.response.redirect("/")
 })
 
-router.get("/login", async ctx => {
+router.get("/login", async (ctx) => {
 	const invalid = ctx.request.url.searchParams.has("invalid")
 	ctx.response.body = render(ctx, <Login invalid={invalid}></Login>)
 	ctx.response.type = "text/html"
 })
 
-router.post("/login", async ctx => {
+router.post("/login", async (ctx) => {
 	const value = await ctx.request.body().value
 	const email = value.get("emailaddress")
 	const password = value.get("password")
@@ -139,7 +139,7 @@ router.post("/login", async ctx => {
 	ctx.response.redirect("/")
 })
 
-router.get("/(.*)", ctx => {
+router.get("/(.*)", (ctx) => {
 	ctx.response.body = render(ctx, <NotFound />)
 	ctx.response.type = "text/html"
 })
@@ -149,7 +149,7 @@ app.use(virtualStorage())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-function App({ children }) {
+function App({ ctx, children }) {
 	return (
 		<html>
 			<head>
@@ -157,7 +157,7 @@ function App({ children }) {
 			</head>
 			<body>
 				<div class="min-h-screen">
-					<NavBar />
+					<NavBar ctx={ctx} />
 					{children}
 				</div>
 			</body>
@@ -165,8 +165,8 @@ function App({ children }) {
 	)
 }
 
-const NavBar = () => {
-	const loggedIn = isLoggedIn()
+const NavBar = ({ ctx }) => {
+	const loggedIn = isLoggedIn(ctx)
 	return (
 		<nav class="font-sans flex text-center flex-row text-left justify-between py-4 px-6 bg-white shadow items-baseline w-full">
 			<div class="mb-0">
@@ -199,12 +199,10 @@ const NavBar = () => {
 }
 
 function Feed({ cheeps }) {
-	console.log(cheeps)
-	const loggedIn = isLoggedIn()
 	return (
 		<div class="flex px-4 justify-center items-center">
 			<div class="">
-				{cheeps.map(cheep => (
+				{cheeps.map((cheep) => (
 					<Cheep cheep={cheep} />
 				))}
 			</div>
